e1d9095d0a3cb54f0914376df575f8b24f3f0852
Chirag Saxena <saxenac7@gmail.com>
2022-01-04 20:50:07 +0530

    Added Header

--- slack-clone/src/Header.js
+++ slack-clone/src/Header.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Header.css';
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime"
 
 function Header() {
     return (
-        <div>
+        <div className='header'>
+             <div className='header_left'>
+             <Avatar className="header_avatar"              alt="Chirag Saxena"
+             />
+             <AccessTimeIcon/>
+             </div>
+             <div className='header_search'></div>
+             <div className='header_right'></div>
+
         </div>
+
+       
     )
 }
 
