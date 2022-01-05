4d82155447c446084e644ee28cfdc4d6c85721b2
Chirag Saxena <saxenac7@gmail.com>
2022-01-05 07:07:49 +0530

    Added Sidebar

--- slack-clone/src/Sidebar.js
+++ slack-clone/src/Sidebar.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import './Sidebar.css'
-
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord"
+import CreateIcon from "@material-ui/icons/Create"
 function Sidebar()
 {
     return(
         <div className="sidebar">
-
+            <div className="sidebar_header">
+                <div className="sidebar_info">
+                    <h2>PenLabs</h2>
+                    <h3>
+                        <FiberManualRecordIcon />
+                        Chirag Saxena
+                    </h3>
+                </div>
+                <CreateIcon />
+            </div>
         </div>
     )
 }
