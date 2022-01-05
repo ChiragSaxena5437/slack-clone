184d6d0844e770e7e98ee3a3c2c8693d5a5e120e
Chirag Saxena <saxenac7@gmail.com>
2022-01-05 17:44:57 +0530

    Added SidebarOption, SidebarOption.js and Sidebar.js

--- slack-clone/src/Sidebar.js
+++ slack-clone/src/Sidebar.js
@@ -2,6 +2,12 @@ import React from 'react'
 import './Sidebar.css'
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord"
 import CreateIcon from "@material-ui/icons/Create"
+import SidebarOption from "./SidebarOption"
+import InsertCommentIcon from "@material-ui/icons/InsertComment"
+import ExpandLessIcon from "@material-ui/icons/ExpandLess"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import AddIcon from "@material-ui/icons/Add"
+
 function Sidebar()
 {
     return(
@@ -14,8 +20,23 @@ function Sidebar()
                         Chirag Saxena
                     </h3>
                 </div>
-                <CreateIcon />
-            </div>
+                    <CreateIcon />
+                </div>
+                    <SidebarOption Icon={InsertCommentIcon} title={"Test"}/>
+                    <SidebarOption  title={"Test1"}/>
+                    <SidebarOption Icon={InsertCommentIcon} title={"Test2"}/>
+                    <SidebarOption Icon={InsertCommentIcon} title={"Test3"}/>
+                    <SidebarOption Icon={InsertCommentIcon} title={"Test4"}/>
+                    <SidebarOption Icon={InsertCommentIcon} title={"Test5"}/>
+                    <SidebarOption Icon={InsertCommentIcon} title={"Test6"}/>
+                    <SidebarOption Icon={InsertCommentIcon} title={"Test7"}/>
+                    <SidebarOption Icon={ExpandLessIcon} title={"Show Less"}/>
+                    <hr />
+                    <SidebarOption Icon={ExpandMoreIcon} title={"Channels"}/>
+                    <SidebarOption Icon={ExpandLessIcon} title={"Show Less"}/>
+                    <hr />
+                    <SidebarOption Icon={AddIcon} addChannelOption title={"Add Channel"}/>
+
         </div>
     )
 }
