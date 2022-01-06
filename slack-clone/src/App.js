dcfe771e214f257ce584a17df0fa4d81477b0f02
Chirag Saxena <saxenac7@gmail.com>
2022-01-06 21:14:59 +0530

    Configured API's

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import {BrowserRouter as Router, Switch ,Route}from "react-router-dom";
 import Chat from "./Chat"
+import Login from "./Login"
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{user}, dispatch] = useStateValue();
   return (
     <div className="app">
       <Router>
+        {!user ? (
+          <Login />
+        ):(
+          
+        <>
         <Header/>
         <div className="app__body">
           <Sidebar />
@@ -20,8 +28,13 @@ function App() {
               <h1>Welcome</h1>
             </Route>
           </Switch>
+
         </div>
+        </>
+        )}
       </Router>
+      
+     
     </div>
   );
 }
--- slack-clone/src/Header.js
+++ slack-clone/src/Header.js
@@ -4,12 +4,18 @@ import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime"
 import SearchIcon from "@material-ui/icons/Search"
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline"
+import {useStateValue} from "./StateProvider"
 
 function Header() {
+    const[{ user }] = useStateValue();
+
+
     return (
         <div className='header'>
              <div className='header_left'>
-             <Avatar className="header_avatar"              alt="Chirag Saxena"
+             <Avatar className="header_avatar"
+              alt={user?.displayname}
+              src={user?.photoURL}
              />
              <AccessTimeIcon/>
              </div>
